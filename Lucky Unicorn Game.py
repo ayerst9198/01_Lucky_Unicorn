85334e2fd32181eb0380b8beca0efd125fe24e89
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2021-02-10 13:14:13 +1300

    Buncha new stuff

--- Lucky Unicorn Game.py
+++ Lucky Unicorn Game.py
@@ -3,13 +3,9 @@ while show_instructions.lower() != "xxx":
     # Ask user if they have played before
     show_instructions = input("Have you played my game before? ").lower()
     # If yes - output 'program continues'
-    if show_instructions == "yes":
+    if show_instructions == "yes" or show_instructions == "y":
         print("program continues")
-    elif show_instructions == "y":
-        print("program continues")
-    elif show_instructions == "no":
-        print("Display Instructions")
-    elif show_instructions == "n":
+    elif show_instructions == "no" or show_instructions == "n":
         print("Display Instructions")
     # If no - output 'display instructions'
     else:
