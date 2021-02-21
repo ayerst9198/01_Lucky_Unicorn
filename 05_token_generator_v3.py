a2e09b814f51c6f6467942b5bae9cdc78ec576c1
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2021-02-22 09:55:04 +1300

    Update 05_token_generator_v3.py

--- 05_token_generator_v3.py
+++ 05_token_generator_v3.py
@@ -10,20 +10,33 @@ for item in range(0,10):
     chosen_num = random.randint(1,100)
 
     # Adjust balance
+    # if the random # is between 1 and 5,
+    # user gets a unicorn (add $4 to the balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
         balance += 4
-    elif 6 <= chosen_num <= 86:
+    # if the random # is between 6 and 36,
+    # user gets a donkey (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
         chosen = "donkey"
         balance -= 1
+    # token is either horse or zebra...
+    # either way subtract $0.5 from balance
     else:
+        # if the number is even, set the chosen
+        # item to horse
         if chosen_num % 2 == 0:
            chosen = "horse"
+        # if the number is odd, set the chosen
+        # item to zebra
         else:
             chosen = "zebra"
         balance -= 0.5
 
-    print("You got a {}. Your balance is"
+    print("You got a {}. Your balance is "
           "${:.2f}".format(chosen, balance))
-
+print()
+print()
+print("Starting Balance = ${}".format(STARTING_BALANCE))
+print("Final Balance = ${}".format(balance))
 print()
